46dde8727949a44c01428eb3fa9115a58c29bf14
david10102016 <dboni595@gmail.com>
2025-12-19 16:43:36 -0400

    actualizar todo

---
+++ js/env.js
@@ -0,0 +1,5 @@
+// Este archivo inyecta las variables de entorno en el navegador
+window.ENV = {
+    VITE_SUPABASE_URL: 'https://knjodbdtzolknptkaaov.supabase.co',
+    VITE_SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtuam9kYmR0em9sa25wdGthYW92Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU5Mjg2NTUsImV4cCI6MjA4MTUwNDY1NX0.zomGZMFd0jSM-zxldB6dYHqIcTY75xGs17beREmGPcM'
+};
